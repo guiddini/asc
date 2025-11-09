a1045274a965f1b74a5ff59d3e942e31b14a9c23
mr-abdellah <belkaid.abdulah@gmail.com>
2025-11-09 21:08:41 +0100

    push

--- src/app/pages/profiles/index.tsx
+++ src/app/pages/profiles/index.tsx
@@ -1,10 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 import { EntityProfilePage } from "./page";
-import ProfileLayout from "./layout";
-import TicketsPage from "./ticket/page";
-import ConfirmTicketPayment from "./ticket/confirm-ticket-payment/page";
-import TicketPaymentResults from "./ticket/ticket-payment-results/page";
-import TickerBoughtSuccess from "./ticket/success-page/page";
 
 const ProfileRoutes = () => {
   return (
@@ -12,16 +7,6 @@ const ProfileRoutes = () => {
       {/* <Route element={<ProfileLayout />}>
       </Route> */}
       <Route path="/:slug" element={<EntityProfilePage />} />
-      <Route path="/:slug/tickets" element={<TicketsPage />} />
-      <Route
-        path="/:slug/tickets/confirm/:orderID"
-        element={<ConfirmTicketPayment />}
-      />
-      <Route
-        path="/:slug/tickets/results/:transactionID"
-        element={<TicketPaymentResults />}
-      />
-      <Route path="/:slug/tickets/success" element={<TickerBoughtSuccess />} />
     </Routes>
   );
 };
