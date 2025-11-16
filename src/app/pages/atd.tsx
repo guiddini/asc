3e430308bde0c4fd64908cf63b72605cab56e39a
mr-abdellah <belkaid.abdulah@gmail.com>
2025-11-16 21:46:37 +0100

    refactor(atd): improve code readab

--- src/app/pages/atd.tsx
+++ src/app/pages/atd.tsx
@@ -2,6 +2,7 @@ import { Navigate, useLocation } from "react-router-dom";
 
 const ATDPage = () => {
   const { search, hash } = useLocation();
+
   const target =
     "/side-events/african-telecommunications-day-2025-qe2kLa" +
     (search || "") +
